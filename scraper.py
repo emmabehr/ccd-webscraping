ec09ddbe189195a8676e7b23b0be668d328f36a9
Emma_Behr <emp.behr@gmail.com>
2021-06-01 21:52:39 +0100

    Added file compare logic

--- scraper.py
+++ scraper.py
@@ -8,4 +8,24 @@ req = Request(url, headers = {"User-Agent": user_agent})
 page = urlopen(req)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-print(html)
+
+file_name = "site.txt"
+try:
+    file = open(file_name, "r")
+except:
+    file = open(file_name,  "w")
+    file.close()
+    file = open(file_name, "r")
+
+old_html = file.read()
+file.close()
+
+compare = old_html == html
+
+if compare:
+    print("No changes detected")
+else: 
+    file = open(file_name, "w")
+    file.write(html)
+    file.close()
+    print("Changes detected")
